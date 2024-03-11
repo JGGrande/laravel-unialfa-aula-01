f9636e8a9c408d86175044294eec6b2f4cf45653
JGGrande <joaogabriel7303@gmail.com>
2024-03-11 19:56:56 -0300

    Adicionado bootstrap ao projeto

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -4,12 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Clientes</title>
 </head>
 <body>
-    <h1>Clientes</h1>
-    @foreach($clients as $client)
-        <h2>{{ $client->name }}</h2>
-    @endforeach
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Home</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        @foreach($clients as $client)
+            <h2 class="text-center">{{ $client->name }}</h2>
+        @endforeach
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
